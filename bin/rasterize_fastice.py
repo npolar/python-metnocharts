172031168285c66d11a7113a9407e3253c9006a1
Mikhail Itkin <mikhail@npolar.no>
2017-03-22 22:39:06 +0100

    OOP'ify (deuglify) rasterization script
    Move shapefile handling outside of the bin scripts and don't expose
    internal stuff to the user. Make it less ugly also.

--- bin/rasterize_fastice.py
+++ bin/rasterize_fastice.py
@@ -2,96 +2,22 @@
 
 import argparse
 
-import geopandas as gpd
-import rasterio
-import fiona
-from rasterio import features
-import os
-from datetime import datetime as dt
-from rasterio.transform import from_origin
-import netCDF4 as nc
-from netCDF4 import netcdftime
-from netcdftime import utime
+from metnocharts.utils import get_timestamp_from_filename
+from metnocharts.charts import get_fastice_from_chart
 
-class GeoTIFFTemplate(object):
-    def __init__(self):
-        self.res = 100
-        self.west    =  225000
-        self.north   = -970000
-        self.south   = -1450000
-        self.east    =  570000
-        self.height = (abs(self.south - self.north) + self.res)/self.res
-        self.width = (abs(self.east - self.west) + self.res)/self.res
-        self.crs = '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
-        self.transform = from_origin(self.west - self.res/2,
-                                     self.north+self.res/2, self.res, self.res)
-        self.shape = (self.height, self.width)
- 
+def main():
 
-def rasterize_layer(convert_to_rast, rst, field, shape=None):
-    # this is where we create a generator of geom, value pairs to use in rasterizing
-    shapes = ((geom,value) for geom, value in zip(convert_to_rast.geometry, convert_to_rast[field]))
-    burned = features.rasterize(shapes=shapes, fill=0, out_shape=shape, transform=rst.transform)
-    return burned
-                
-def create_netcdf_file(fname, shape, crs=None, north=None, west=None):
-    """Create a netCDF file with variable dimensions *shape*
-    Args:
-      fname (str) : output filename
-      shape (tuple) : data array dimensions
-    Returns:
-      root_dst (netCDF4.Dataset)  : dataset open for writing
-    """
-    root_dst = nc.Dataset(fname, 'w', format='NETCDF4')
-    if crs is not None:
-        root_dst.crs = crs
-    root_dst.x_upper_left = west
-    root_dst.y_upper_left = north
-
-    # create dimensions, we assume the data is always 3d
-    root_dst.createDimension('time', None)
-    root_dst.createDimension('y', shape[1])
-    root_dst.createDimension('x', shape[0])
-    
-    # create variables
-    root_dst.createVariable('time', 'i4', 
-                            dimensions=('time'))
-    root_dst.createVariable('ice_concentration', 'i4', 
-                            dimensions=('time', 'x', 'y'),
-                            zlib=True)
-    root_dst.variables['time'].units = 'Days since 2000-01-01'
-    return root_dst
-
-def get_timestamp_from_filename(fname):
-    '''Parse basename and return cdf time'''
-    date = dt.strptime(fname, 'ice%Y%m%d.shp')
-    cdftime = utime('days since 2000-01-01')
-    timestamp = cdftime.date2num(date)
-    return timestamp
-
-
-if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--ifile", "-i")
     parser.add_argument("--ofile", "-o")
-    parser.add_argument("--template", "-t")
     args = parser.parse_args()
 
     ifile = args.ifile
     ofile = args.ofile
-    tif_template = args.template
 
-    rst = rasterio.open(tif_template)
-    convert_to_rast = gpd.read_file(ifile) 
-    convert_to_rast = convert_to_rast.loc[convert_to_rast['ICE_TYPE'] == 'Fast Ice']
-    convert_to_rast['Junk'] = 1    
-    if rst.crs != convert_to_rast.crs:
-         convert_to_rast = convert_to_rast.to_crs(rst.crs)
-    template = GeoTIFFTemplate()
-    data = rasterize_layer(convert_to_rast, rst, 'Junk', shape=template.shape)
-    out_dst = create_netcdf_file(ofile, data.shape, crs=template.crs,
-            west=template.west, north=template.north)
-    out_dst.variables['time'][:] = timestamp 
-    out_dst.variables['ice_concentration'][0,:,:] = data 
+    fastice_dst = get_fastice_from_chart(ifile)
+    fastice_dst.rasterize()
+    fastice_dst.save_netcdf(ofile)
 
-    out_dst.close()
+if __name__ == '__main__':
+    main()
--- metnocharts/charts.py
+++ metnocharts/charts.py
@@ -1,17 +1,44 @@
+import numpy as np
 import netCDF4 as nc
+import geopandas as gpd
+from rasterio import features
+from rasterio import transform
+
 from metnocharts.utils import get_timestamp_from_filename
+
 
 class IceChartDataset(object):
     def __init__(self, fname):
         self.fname = fname
         self.read_dst(fname)
+        self.setup_geotransform()
+
+    def setup_geotransform(self):
+        self.xres = 100
+        self.yres = 100
+        self.west    =  225000
+        self.north   = -970000
+        self.south   = -1450000
+        self.east    =  570000
+        self.height = (abs(self.south - self.north) + self.yres)/self.yres
+        self.width = (abs(self.east - self.west) + self.xres)/self.xres
+
+        self.transform = transform.from_origin(self.west,
+                                               self.north,
+                                               self.width,
+                                               self.height)
     
     def rasterize(self):
-        self.ice_conc = None
+        self.read_shp()
+        self.ice_conc = features.rasterize(self.shapes, 
+                                           out_shape=(self.height, self.width),
+                                           fill=0,
+                                           transform=transform,
+                                           dtype='int16')
 
     def save_netcdf(self, fname):
-        nc_filehandle = create_netcdf_chart(fname, self.crs)
-        nc_filehandle['ice_concentration'][:] = self.ice_conc
+        nc_filehandle = self.create_netcdf_chart(fname)
+        nc_filehandle['ice_concentration'][0,:] = self.ice_conc
         nc_filehandle['time'][:] = self.timestamp
         nc_filehandle.close()
 
@@ -21,7 +48,37 @@ class IceChartDataset(object):
         self.read_shp()
 
     def read_shp(self):
-        pass
+        shp_dst = gpd.read_file(self.fname) 
+        shp_dst = shp_dst.loc[shp_dst['ICE_TYPE'] == 'Fast Ice']
+        self.shapes = shp_dst
+
+    def create_netcdf_chart(self, fname):
+        """Create a netCDF file"""
+        root_dst = nc.Dataset(fname, 'w', format='NETCDF4')
+        if self.crs is not None:
+            root_dst.crs = self.crs
+
+
+        root_dst.x_upper_left = self.west
+        root_dst.y_upper_left = self.north
+
+        # create dimensions, we assume the data is always 3d
+        root_dst.createDimension('time', None)
+        root_dst.createDimension('y', self.height)
+        root_dst.createDimension('x', self.width)
+        
+        # create variables
+        root_dst.createVariable('time',
+                                'i4', 
+                                dimensions=('time'))
+
+        root_dst.createVariable('ice_concentration',
+                                'i4', 
+                                dimensions=('time', 'x', 'y'),
+                                zlib=True)
+
+        root_dst.variables['time'].units = 'Days since 2000-01-01'
+        return root_dst
 
 def get_crs(fname):
     crs = '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
@@ -31,36 +88,3 @@ def get_fastice_from_chart(fname):
     fastice_dst = IceChartDataset(fname)
     return fastice_dst
 
-def create_netcdf_chart(fname, crs):
-    """Create a netCDF file"""
-    root_dst = nc.Dataset(fname, 'w', format='NETCDF4')
-    if crs is not None:
-        root_dst.crs = crs
-
-    xres = 100
-    yres = 100
-
-    west    =  225000
-    north   = -970000
-    south   = -1450000
-    east    =  570000
-
-    height = (abs(south - north) + yres)/yres
-    width = (abs(east - west) + xres)/xres
-
-    root_dst.x_upper_left = west
-    root_dst.y_upper_left = north
-
-    # create dimensions, we assume the data is always 3d
-    root_dst.createDimension('time', None)
-    root_dst.createDimension('y', height)
-    root_dst.createDimension('x', width)
-    
-    # create variables
-    root_dst.createVariable('time', 'i4', 
-                            dimensions=('time'))
-    root_dst.createVariable('ice_concentration', 'i4', 
-                            dimensions=('time', 'x', 'y'),
-                            zlib=True)
-    root_dst.variables['time'].units = 'Days since 2000-01-01'
-    return root_dst
