0713f9ebd7512c3d54e3888bfc1d7b3cc61d8ed3
Mikhail Itkin <mikhail@npolar.no>
2017-03-23 09:08:55 +0100

    Make constructor read dataset

--- metnocharts/charts.py
+++ metnocharts/charts.py
@@ -12,6 +12,9 @@ class IceChartDataset(object):
         self.fname = fname
         self.read_dst(fname)
         self.setup_geotransform()
+        self.ice_conc = None
+        self.shapes = None
+        self.crs = get_crs()
 
     def setup_geotransform(self):
         self.xres = 100
@@ -23,28 +26,32 @@ class IceChartDataset(object):
         self.height = (abs(self.south - self.north) + self.yres)/self.yres
         self.width = (abs(self.east - self.west) + self.xres)/self.xres
 
-        self.transform = transform.from_origin(self.west,
-                                               self.north,
-                                               self.width,
-                                               self.height)
+        self.geotransform = transform.from_origin(self.west-self.xres/2,
+                                               self.north+self.xres/2,
+                                               self.xres,
+                                               self.yres)
     
     def rasterize(self):
-        self.read_shp()
-        self.ice_conc = features.rasterize(self.shapes, 
-                                           out_shape=(self.height, self.width),
-                                           fill=0,
-                                           transform=transform,
-                                           dtype='int16')
+        if self.shapes is None:
+            self.read_shp()
+
+        self.shapes['Junk']=1
+        self.shapes.to_crs(self.crs)
+        shapes = ((geom,value) for geom, value in zip(self.shapes.geometry,
+                                                      self.shapes['Junk']))
+        self.ice_conc = features.rasterize(shapes, 
+                                           out_shape=(self.height, self.width), fill=0,
+                                           transform=self.geotransform)
 
     def save_netcdf(self, fname):
-        nc_filehandle = self.create_netcdf_chart(fname)
-        nc_filehandle['ice_concentration'][0,:] = self.ice_conc
-        nc_filehandle['time'][:] = self.timestamp
-        nc_filehandle.close()
+        if self.ice_conc is not None:
+            nc_filehandle = self.create_netcdf_chart(fname)
+            nc_filehandle['ice_concentration'][0,:,:] = self.ice_conc
+            nc_filehandle['time'][:] = self.timestamp
+            nc_filehandle.close()
 
     def read_dst(self, fname):
         self.timestamp = get_timestamp_from_filename(self.fname)
-        self.crs = get_crs(fname)
         self.read_shp()
 
     def read_shp(self):
@@ -57,8 +64,6 @@ class IceChartDataset(object):
         root_dst = nc.Dataset(fname, 'w', format='NETCDF4')
         if self.crs is not None:
             root_dst.crs = self.crs
-
-
         root_dst.x_upper_left = self.west
         root_dst.y_upper_left = self.north
 
@@ -71,16 +76,14 @@ class IceChartDataset(object):
         root_dst.createVariable('time',
                                 'i4', 
                                 dimensions=('time'))
-
         root_dst.createVariable('ice_concentration',
                                 'i4', 
-                                dimensions=('time', 'x', 'y'),
+                                dimensions=('time', 'y', 'x'),
                                 zlib=True)
-
         root_dst.variables['time'].units = 'Days since 2000-01-01'
         return root_dst
 
-def get_crs(fname):
+def get_crs():
     crs = '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
     return crs
 
