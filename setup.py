92855179888f6b1bc0f1f81a78cd9274ec20d3f1
Mikhail Itkin <mikhail@npolar.no>
2017-04-03 11:56:15 +0200

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 import setuptools
 from metnocharts.version import version
 
-
-setuptools.setup(name='python-metnocharts',
+setuptools.setup(name='metnocharts',
                  version=version,
                  description='Python Package for Met Norway Ice charts processing',
                  long_description=open('README.rst').read().strip(),
                  author='Mikhail Itkin',
                  author_email='itkin.m@gmail.com',
                  py_modules=['metnocharts'],
-                 install_requires=[],
+                 install_requires=['numpy',
+                                    'pandas',
+                                    'netCDF4',
+                                   'rasterio',
+                                   'geopandas'],
                  license='GPLv3',
                  zip_safe=False,
                  keywords='sea ice charts',
