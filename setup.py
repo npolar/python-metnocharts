5d449d412ca08e89e7278cb190ef55b7b2ef1fd0
Mikhail Itkin <mikhail@npolar.no>
2017-04-03 12:38:21 +0200

    Make package importable

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 import setuptools
 from metnocharts.version import version
 
-setuptools.setup(name='metnocharts',
+setuptools.setup(name='python-metnocharts',
                  version=version,
                  description='Python Package for Met Norway Ice charts processing',
                  long_description=open('README.rst').read().strip(),
                  author='Mikhail Itkin',
                  author_email='itkin.m@gmail.com',
                  py_modules=['metnocharts'],
+                 packages = ['metnocharts'],
                  install_requires=['numpy',
                                     'pandas',
                                     'netCDF4',
