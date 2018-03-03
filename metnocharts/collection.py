6cde00a9877dbd350b8a8384a7dd5bc132135a2c
Mikhail Itkin <itkin.m@gmail.com>
2018-03-03 16:01:01 +0100

    Make collection retrieve data + export
    Collection is able to pull data from Postgresql DB
    Results can be exported to geopandas dataframe for further
    processing

--- metnocharts/collection.py
+++ metnocharts/collection.py
@@ -1,3 +1,6 @@
+import geopandas as gpd
+
+
 class ChartsCollection(object):
     def __init__(self, dbname, username, password, dbtype=None):
         self.collection = None
@@ -7,7 +10,7 @@ class ChartsCollection(object):
         self.dbtype = dbtype
         self._connection = None
         self._make_connection_str()
-        
+
     def _make_connection_str(self):
         self._conn_str = "host='localhost' dbname='{}' user='{}' password='{}' port='5433'".format(
                 self.dbname,
@@ -33,11 +36,40 @@ class ChartsCollection(object):
         try:
             if self.dbtype == 'postgres':
                  self._connect_to_posgres()
+            elif self.dbtype is None:
+                raise TypeError('Database type is None')
+            else:
+                raise TypeError("Don't know which database type to connect to")
         except:
             raise ConnectionError('Could not connect to the datebase {}'.format(
                 self.dbname))
 
-    def retrieve(self, query):
+    def retrieve_query(self, query):
         if not self._connection:
             self._connect()
-        return self 
+        cur = self._connection.cursor()
+        cur.execute(query)
+        return cur.fetchall()
+
+    def _retrieve_table_stats(self, tablename):
+        self.cursor = self._connection.cursor()
+        query = ' '.join(("SELECT attname, n_distinct FROM pg_stats",
+                 "WHERE tablename='{}' and".format(tablename),
+                 "attname='gid'"))
+        self.cursor.execute(query)
+        results = self.cursor.fetchall()
+        return results
+
+    def to_gpd(self, query, crs=None, geom_col=None):
+        if geom_col == None:
+            geom_col = 'geom'
+        df = gpd.GeoDataFrame.from_postgis(query,
+                                           self._connection,
+                                           geom_col=geom_col)
+        if df.crs is None:
+            crs = {'init':'epsg:4326'}
+            df.crs = crs
+
+        if crs != df.crs:
+            df[geom_col] = df[geom_col].to_crs(crs)
+        return df
--- tests/test_collection.py
+++ tests/test_collection.py
@@ -6,18 +6,25 @@ from metnocharts.collection import ChartsCollection
 class TestCollection(TestCase):
     def setUp(self):
         dbname = 'foo'
-        username = 'bar'
+        username = 'user'
         password = 'charts'
         dbtype = 'postgres'
         self.charts_collection = ChartsCollection(dbname,
                                              username,
                                              password,
                                              dbtype=dbtype)
-    
+
+        self.test_query = ' '.join(("SELECT * FROM",
+                          "merged_charts",
+                          "where dates = '2017-01-30'"))
+
+    def tearDown(self):
+        self.charts_collection = None
+
     def test_connection(self):
         self.charts_collection._connect()
         self.assertIsNotNone(self.charts_collection._connection)
-        
+
     @unittest.skip('skip')
     def test_retrieves_collection(self):
         result = self.charts_collection.retrieve()
@@ -26,3 +33,28 @@ class TestCollection(TestCase):
     def test_connect_to_postgres(self):
         self.charts_collection._connect_to_posgres()
         self.assertIsNotNone(self.charts_collection._connection)
+
+    def test_get_table_stats(self):
+        self.charts_collection._connect()
+        self.charts_collection._retrieve_table_stats('merged_charts')
+
+    def test_database_type_not_known(self):
+        self.charts_collection.dbtype = None
+        with self.assertRaises(ConnectionError):
+            self.charts_collection._connect()
+
+    def test_database_type_is_known(self):
+        self.charts_collection.dbtype = 'postgres'
+        self.charts_collection._connect()
+        self.assertIsNotNone(self.charts_collection._connection)
+
+    def test_retrieve_all(self):
+        query = ' '.join(("SELECT * FROM",
+                          "merged_charts",
+                          "where dates = '2017-01-30'"))
+
+        self.charts_collection.retrieve_query(query)
+
+    def test_to_geodataframe(self):
+        self.charts_collection._connect()
+        df = self.charts_collection.to_gpd(self.test_query, crs={"init":"epsg:3411"})
